0c6c563835ad6cd94a0cc771326afb9c9300280a
joewstroman <joewstroman@gmail.com>
2018-11-27 21:02:18 -0500

    Import styles as a module

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCaretLeft, faCaretRight, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { appStyles, boardStyles, cardStyles, getRandomColor, headerStyles, iconStyles } from './Styles.js'
 import './App.css';
 
 class App extends Component {
@@ -360,59 +361,6 @@ class Card extends Component {
         </div>
       )
   }
-}
-
-const getRandomColor = () => {
-  let red = (135 + Math.floor(Math.random() * 120)).toString(16);
-  let green = (135 + Math.floor(Math.random() * 120)).toString(16);
-  let blue = (200 + Math.floor(Math.random() * 55)).toString(16);
-
-  return `#${red}${green}${blue}D6`;
-}
-
-const iconStyles = () => {
-  return {cursor: "pointer", fontSize: "13px"}
-}
-
-const headerStyles = (color) => {
-  let styles = {
-    backgroundColor: color,
-    color: "white",
-    margin: "0"
-  }
-  return styles;
-}
-
-const cardStyles = () => {
-  return { 
-    padding: "5px 0px",
-    backgroundColor: "rgb(242,242,242)"
-  }
-}
-
-const boardStyles = () => {
-  let styles = { 
-    margin: "5px 0px",
-    backgroundColor: "rgb(242,242,242)",
-    textAlign: "center", 
-    minWidth: 300,
-    border: 1,
-    borderStyle: "groove",
-    borderRadius: "5px",
-    overflow: "hidden"
-  }
-  return styles;
-}
-
-const appStyles = () => {
-  let styles = {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    justifyContent: 'space-around',
-    padding: 25 / 2,
-  }
-  return styles;
 }
 
 export default App;
